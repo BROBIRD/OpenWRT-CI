259208f9433cb18c0f8d34413f45fc8a640513d1
BROBIRD <brobirdcn@gmail.com>
2020-11-12 23:57:54 +0800

    update and add x86_64

---
+++ x86_64-diy-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/extra/luci-app-adguardhome
+git clone https://github.com/sensec/luci-app-udp2raw.git package/extra/luci-app-udp2raw
+sed -i '65,68d' package/extra/luci-app-udp2raw/Makefile
+git clone https://github.com/sxml/openwrt-udp2raw.git package/extra/openwrt-udp2raw
+git clone https://github.com/haodong/luci-app-speederv2.git package/extra/luci-app-speederv2
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
---
+++ x86_64-diy-part2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+sed -i '/--dport 53 -j REDIRECT --to-ports 53/d' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/\$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF.:0/\$1\$oYZwH1vw$DIG7XTG6XBboLcS0wsM9z0:18039/g' package/lean/default-settings/files/zzz-default-settings
+sed -i -r '/elseif szType == ("ssd"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
