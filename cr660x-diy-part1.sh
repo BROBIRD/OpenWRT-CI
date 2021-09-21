d2f0f4c530b45971d230bfab72cfa5941fc3abab
BROBIRD <brobirdcn@gmail.com>
2021-09-22 00:34:26 +0800

    Create Config for Xiaomi CR660x

---
+++ cr660x-diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
