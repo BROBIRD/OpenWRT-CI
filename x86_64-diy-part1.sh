675a6a042a78f79b0ab1a16923ab44f2b111089e
BROBIRD <brobirdcn@gmail.com>
2022-01-07 21:09:53 +0800

    fix luci-app-adguardhome

--- x86_64-diy-part1.sh
+++ x86_64-diy-part1.sh
@@ -15,8 +15,8 @@ sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 
-git clone https://github.com/Hyy2001X/luci-app-adguardhome.git package/extra/luci-app-adguardhome
-sed -i '10i\PKG_BUILD_DEPENDS += lua/host luci-base/host' package/extra/luci-app-adguardhome/Makefile
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/extra/luci-app-adguardhome
+#sed -i '10i\PKG_BUILD_DEPENDS += lua/host luci-base/host' package/extra/luci-app-adguardhome/Makefile
 git clone https://github.com/Andylive5518/luci-app-udp2raw.git package/extra/luci-app-udp2raw
 sed -i '65,68d' package/extra/luci-app-udp2raw/Makefile
 git clone https://github.com/txyiezero/openwrt-udp2raw.git package/extra/openwrt-udp2raw
