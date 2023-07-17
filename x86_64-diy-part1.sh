cffbdc0b140c49b1ce55bc9fd00b1894706e88ec
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2023-07-17 11:01:00 +0800

    update x86-64 diy-part1

--- x86_64-diy-part1.sh
+++ x86_64-diy-part1.sh
@@ -20,20 +20,20 @@ sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/extra/luci-app-adguardhome
 sed -i '11i\PKG_BUILD_DEPENDS += lua/host luci-base/host' package/extra/luci-app-adguardhome/Makefile
-git clone https://github.com/Andylive5518/luci-app-udp2raw.git package/extra/luci-app-udp2raw
-sed -i '65,68d' package/extra/luci-app-udp2raw/Makefile
-git clone https://github.com/txyiezero/openwrt-udp2raw.git package/extra/openwrt-udp2raw
-git clone https://github.com/haodong/luci-app-speederv2.git package/extra/luci-app-speederv2
-curl -sL -m 30 --retry 2 https://github.com/wangyu-/UDPspeeder/releases/latest/download/speederv2_binaries.tar.gz -o /tmp/speederv2_binaries.tar.gz
-tar zxvf /tmp/speederv2_binaries.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/speederv2_amd64 >/dev/null 2>&1
-mkdir -p package/extra/luci-app-speederv2/files/root/usr/bin
-mv /tmp/speederv2_amd64 package/extra/luci-app-speederv2/files/root/usr/bin/speederv2 >/dev/null 2>&1
-rm -rf /tmp/speederv2_binaries.tar.gz >/dev/null 2>&1
-sed -i '71i\	$(INSTALL_DIR) $(1)/usr/bin' package/extra/luci-app-speederv2/Makefile
-sed -i '72i\	$(INSTALL_BIN) ./files/root/usr/bin/speederv2 $(1)/usr/bin/speederv2' package/extra/luci-app-speederv2/Makefile
+# git clone https://github.com/Andylive5518/luci-app-udp2raw.git package/extra/luci-app-udp2raw
+# sed -i '65,68d' package/extra/luci-app-udp2raw/Makefile
+# git clone https://github.com/txyiezero/openwrt-udp2raw.git package/extra/openwrt-udp2raw
+# git clone https://github.com/haodong/luci-app-speederv2.git package/extra/luci-app-speederv2
+# curl -sL -m 30 --retry 2 https://github.com/wangyu-/UDPspeeder/releases/latest/download/speederv2_binaries.tar.gz -o /tmp/speederv2_binaries.tar.gz
+# tar zxvf /tmp/speederv2_binaries.tar.gz -C /tmp >/dev/null 2>&1
+# chmod +x /tmp/speederv2_amd64 >/dev/null 2>&1
+# mkdir -p package/extra/luci-app-speederv2/files/root/usr/bin
+# mv /tmp/speederv2_amd64 package/extra/luci-app-speederv2/files/root/usr/bin/speederv2 >/dev/null 2>&1
+# rm -rf /tmp/speederv2_binaries.tar.gz >/dev/null 2>&1
+# sed -i '71i\	$(INSTALL_DIR) $(1)/usr/bin' package/extra/luci-app-speederv2/Makefile
+# sed -i '72i\	$(INSTALL_BIN) ./files/root/usr/bin/speederv2 $(1)/usr/bin/speederv2' package/extra/luci-app-speederv2/Makefile
 
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
 
 mkdir package/extra/luci-app-openclash
 cd package/extra/luci-app-openclash
