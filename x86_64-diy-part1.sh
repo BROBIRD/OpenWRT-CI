729c4eaf6cd6d02f2f28de298c882abf5dfa25d8
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2024-07-23 01:27:29 +0800

    remove tinyproxy and add gost

--- x86_64-diy-part1.sh
+++ x86_64-diy-part1.sh
@@ -11,6 +11,7 @@
 # Uncomment a feed source
 sed -i '$a\src-git helloworld https://github.com/fw876/helloworld;main' feeds.conf.default
 # sed -i '$a\src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a\src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
