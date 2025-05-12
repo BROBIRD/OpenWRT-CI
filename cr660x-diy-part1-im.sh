6d8f4861c4416fc5f0d1debf1c6c9e02239201a4
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-05-12 08:32:28 +0800

    初步加入immortal固件编译

---
+++ cr660x-diy-part1-im.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+sed -i '$a\src-git helloworld https://github.com/fw876/helloworld;main' feeds.conf.default
+# sed -i '$a\src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/extra/luci-app-adguardhome
+sed -i '11i\PKG_BUILD_DEPENDS += lua/host luci-base/host' package/extra/luci-app-adguardhome/Makefile
---
+++ cr660x-diy-part2-im.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+sed -i 's/192.168.1/192.168.2/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Linux/g' package/base-files/files/bin/config_generate
+sed -i -r '/elseif szType == ("sip008"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+
+#只用ipv4地址订阅及更新
+sed -i "s/wget -q/wget -4 -q/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+sed -i "s/wget --no-check-certificate/wget -4 --no-check-certificate/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
+
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/extra/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/extra/luci-app-argon-config
+sed -i '110d' feeds/packages/libs/qtbase/Makefile
+# rm -rf target/linux/ramips/patches-5.4/999-fix-hwnat.patch
+# rm -rf target/linux/ramips/patches-5.10/999-fix-hwnat.patch
