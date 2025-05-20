f11ba8cb8e53a8fd8367f966979669e3306ddb43
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-05-21 03:59:02 +0800

    update

--- cr660x-diy-part2-im.sh
+++ cr660x-diy-part2-im.sh
@@ -12,10 +12,13 @@
 sed -i 's/192.168.1/192.168.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/Linux/g' package/base-files/files/bin/config_generate
 sed -i -r '/elseif szType == ("sip008"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+sed -i -r '/elseif szType == ("sip008"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/smpackage/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
 
 #只用ipv4地址订阅及更新
 sed -i "s/wget -q/wget -4 -q/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
 sed -i "s/wget --no-check-certificate/wget -4 --no-check-certificate/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
+sed -i "s/wget -q/wget -4 -q/" feeds/smpackage/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+sed -i "s/wget --no-check-certificate/wget -4 --no-check-certificate/" feeds/smpackage/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
 
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/extra/luci-theme-argon
