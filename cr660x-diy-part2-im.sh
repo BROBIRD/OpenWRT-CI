844a330330634aea138fa641f90829ce6d2f3abd
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-06-07 15:42:00 +0800

    update

--- cr660x-diy-part2-im.sh
+++ cr660x-diy-part2-im.sh
@@ -133,9 +133,11 @@ sed -i "s/-O3/-Ofast/g" package/libs/openssl/Makefile
 # sed -i '/libssl.so/a\	$(LN) libssl.so.3 $(1)/usr/lib/libssl.so.1.1' package/libs/openssl/Makefile
 
 # nghttp3
+rm -rf feeds/packages/libs/nghttp3
 git clone https://github.com/sbwml/package_libs_nghttp3 package/libs/nghttp3
 
 # ngtcp2
+rm -rf feeds/packages/libs/ngtcp2
 git clone https://github.com/sbwml/package_libs_ngtcp2 package/libs/ngtcp2
 
 # curl - http3/quic
