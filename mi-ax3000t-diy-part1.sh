b85a9bbab641ca08c02c698fb559bf5cc160e952
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-09-19 23:16:24 +0800

    update

--- mi-ax3000t-diy-part1.sh
+++ mi-ax3000t-diy-part1.sh
@@ -8,6 +8,18 @@
 # Blog: https://p3terx.com
 #=============================================================
 
+# 启用旧版Luci
+sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
+sed -i '/src-git.*openwrt-23/s/^/#/' feeds.conf.default
+sed -i '/src-git.*openwrt-24/s/^/#/' feeds.conf.default
+
+sed -i '$a\src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+
+sed -i 's/+luci-compat/+luci-base/g' package/lean/default-settings/Makefile
+
+
+
 # Uncomment a feed source
 sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # sed -i '$a\src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
@@ -17,3 +29,5 @@ sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/extra/luci-app-jd-dailybonus
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/extra/luci-app-adguardhome
 sed -i '11i\PKG_BUILD_DEPENDS += lua/host luci-base/host' package/extra/luci-app-adguardhome/Makefile
+
+git clone https://github.com/EasyTier/luci-app-easytier.git package/extra/easytier
