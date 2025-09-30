38a4b53a29cec5b75d3e62a153b898a338448cb4
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-09-30 23:53:38 +0800

    add zn-m5 and mi-ax3000t switch to close-source driver

--- zn-m5-diy-part2.sh
+++ zn-m5-diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1/192.168.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1/10.0.2/g' package/base-files/files/bin/config_generate
 sed -i '/--dport 53 -j REDIRECT --to-ports 53/d' package/lean/default-settings/files/zzz-default-settings
 sed -i -r '/elseif szType == ("sip008"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
 
@@ -18,8 +18,8 @@ sed -i "s/wget -q/wget -4 -q/" feeds/helloworld/luci-app-ssr-plus/root/usr/share
 sed -i "s/wget --no-check-certificate/wget -4 --no-check-certificate/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
 
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/extra/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/extra/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/extra/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/extra/luci-app-argon-config
 # sed -i '110d' feeds/packages/libs/qtbase/Makefile
 # rm -rf target/linux/ramips/patches-5.4/999-fix-hwnat.patch
 # rm -rf target/linux/ramips/patches-5.10/999-fix-hwnat.patch
