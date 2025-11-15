57fc8bf427d50566737192c28b70955918c18acc
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-11-16 02:38:23 +0800

    Fix regex pattern in subscribe.lua script

--- x86_64-diy-part2.sh
+++ x86_64-diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 sed -i '/--dport 53 -j REDIRECT --to-ports 53/d' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/\$1\$V4UetPzk\$CYXluq4wUazHjmCDBCqXF.:0/\$1\$oYZwH1vw$DIG7XTG6XBboLcS0wsM9z0:18039/g' package/lean/default-settings/files/zzz-default-settings
-sed -i -r '/elseif szType == ("sip008"|'\''vmess'\'') then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+sed -i -r '/elseif szType == ("sip008"|"vmess") then/i\\t\tresult.fast_open = "1"' feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
 
 #只用ipv4地址订阅及更新
 sed -i "s/wget -q/wget -4 -q/" feeds/helloworld/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
