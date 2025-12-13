7c5bc006249546e7f0451d37fd5022306e8fed03
BROBIRD <7692707+BROBIRD@users.noreply.github.com>
2025-12-13 16:46:06 +0800

    update

--- x86_64-diy-part1.sh
+++ x86_64-diy-part1.sh
@@ -15,6 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 sed -i '/src-git.*openwrt-23/s/^/#/' feeds.conf.default
 sed -i '/src-git.*openwrt-24/s/^/#/' feeds.conf.default
+sed -i '/src-git.*openwrt-25/s/^/#/' feeds.conf.default
 
 sed -i '$a\src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,tcping}
